7adebd483cfbe792db6bbbdf424a87d99cdc0b9e
Kenichi Sakabe <saken649@gmail.com>
2017-11-28 21:36:56 +0900

    プロジェクト ファイルを追加します。

---
+++ PageControlTest2/PageControlTest2/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PageControlTest2
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            btnNextPage.Clicked += (sender, e) =>
+            {
+                this.Navigation.PushAsync(new SecondPage());
+            };
+		}
+	}
+}
